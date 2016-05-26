2f643c8d06e0bc912ccb925f61a86f106e497904
alanchung <alanchung@alans-imac.home>
2016-05-26 00:05:27 -0400

    first checkin

---
+++ src/com/tutorialspoint/MainApp_Injecting_Collection.java
@@ -0,0 +1,19 @@
+package com.tutorialspoint;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp_Injecting_Collection {
+   public static void main(String[] args) {
+      @SuppressWarnings("resource")
+	ApplicationContext context = 
+             new ClassPathXmlApplicationContext("Beans.xml");
+
+      JavaCollection jc=(JavaCollection)context.getBean("javaCollection");
+
+      jc.getAddressList();
+      jc.getAddressSet();
+      jc.getAddressMap();
+      jc.getAddressProp();
+   }
+}
